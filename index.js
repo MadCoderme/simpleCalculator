b177cda36b45e6ac89eefeb029c5cf2a3a36917b
MadCoderme <63955762+MadCoderme@users.noreply.github.com>
2022-06-29 22:26:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send(req.query.f + ' ' + req.query.o + ' ' + req.query.s)
+  let f = req.query.f
+  let o = req.query.o
+  let s = req.query.s
+  var res = 0
+  
+  switch(o) {
+    case 'add':
+      res = f + s
+      break
+    case 'minus":
+      res = f - s
+      break
+    default:
+      break
+  }
+  
+  res.send(res)
 })
 
 app.listen(port, () => {
